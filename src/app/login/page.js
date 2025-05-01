53d962fd77ee5d751fc21bfd382b4b4edfca0d45
MandaDaniel <rajaonariherymandadaniel@gmail.com>
2025-05-02 01:58:02 +0300

    marche locale

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,13 @@
 "use client";
 
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Box } from "@mui/material";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import dynamic from "next/dynamic";
-
-// Chargement dynamique pour éviter les erreurs côté serveur
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
-
+import Lottie from "lottie-react";
+import loginAnimation from "@/animations/login.json";
+import signupAnimation from "@/animations/signup.json";
 
 const LoginPage = () => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -17,21 +15,13 @@ const LoginPage = () => {
   const [password, setPassword] = useState(""); // Valeur statique pour le mot de passe
   const [fullName, setFullName] = useState("");
   const [error, setError] = useState("");
+  const [isClient, setIsClient] = useState(false); // État pour vérifier si c'est côté client
   const router = useRouter();
-  const [loginData, setLoginData] = useState(null);
-  const [signupData, setSignupData] = useState(null);
 
   const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
-
   useEffect(() => {
-    fetch("/animations/login.json")
-      .then((res) => res.json())
-      .then(setLoginData);
-  
-    fetch("/animations/signup.json")
-      .then((res) => res.json())
-      .then(setSignupData);
+    setIsClient(true); // On définit l'état lorsque le composant est monté côté client
   }, []);
 
   const handleSubmit = () => {
@@ -39,24 +29,23 @@ const LoginPage = () => {
       setError("Veuillez remplir tous les champs.");
       return;
     }
-  
+
     // Blocage de la fonctionnalité d'inscription
     if (isSignUp) {
       setError("Inscription désactivée pour le moment.");
       return;
     }
-  
+
     // Vérification statique des identifiants
     if (email === "mandarajaonarihery@gmail.com" && password === "1234") {
       localStorage.setItem("userEmail", email);
       localStorage.setItem("userRole", "med");
-  
+
       router.push("/med");
     } else {
       setError("Identifiants incorrects.");
     }
   };
-  
 
   return (
     <Box
@@ -68,48 +57,47 @@ const LoginPage = () => {
       }}
     >
       {/* Animation section */}
-      <motion.div
-        initial={{ x: 0 }}
-        animate={{ x: isSignUp ? "100%" : "0%" }}
-        transition={{ duration: 0.8 }}
-        style={{
-          flex: 1,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "linear-gradient(135deg, #6a11cb, #2575fc)",
-          position: "relative",
-          padding: "2rem",
-        }}
-      >
-        <Box
-          sx={{
-            width: { xs: "100%", sm: "90%", md: "80%" },
-            height: { xs: "100%", sm: "90%", md: "80%" },
-            maxWidth: "600px",
-            maxHeight: "600px",
+      {isClient && (
+        <motion.div
+          initial={{ x: 0 }}
+          animate={{ x: isSignUp ? "100%" : "0%" }}
+          transition={{ duration: 0.8 }}
+          style={{
+            flex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "linear-gradient(135deg, #6a11cb, #2575fc)",
+            position: "relative",
+            padding: "2rem",
           }}
         >
-        {(isSignUp ? signupData : loginData) && (
-  <Lottie animationData={isSignUp ? signupData : loginData} loop />
-)}
-
-        </Box>
-        <Typography
-          variant="h3"
-          sx={{
-            zIndex: 1,
-            textAlign: "center",
-            fontWeight: "bold",
-            color: "#fff",
-            padding: 2,
-            position: "absolute",
-            bottom: "20px",
-          }}
-        >
-          {isSignUp ? "Rejoignez-nous dès aujourd'hui !" : "Bon retour !"}
-        </Typography>
-      </motion.div>
+          <Box
+            sx={{
+              width: { xs: "100%", sm: "90%", md: "80%" },
+              height: { xs: "100%", sm: "90%", md: "80%" },
+              maxWidth: "600px",
+              maxHeight: "600px",
+            }}
+          >
+            <Lottie animationData={isSignUp ? signupAnimation : loginAnimation} loop />
+          </Box>
+          <Typography
+            variant="h3"
+            sx={{
+              zIndex: 1,
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "#fff",
+              padding: 2,
+              position: "absolute",
+              bottom: "20px",
+            }}
+          >
+            {isSignUp ? "Rejoignez-nous dès aujourd'hui !" : "Bon retour !"}
+          </Typography>
+        </motion.div>
+      )}
 
       {/* Formulaire */}
       <motion.div
