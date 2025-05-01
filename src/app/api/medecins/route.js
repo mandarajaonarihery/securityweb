d8d597f3392b25bd8b444e497fbb7ada7b7f7bc6
MandaDaniel <rajaonariherymandadaniel@gmail.com>
2025-05-02 01:07:34 +0300

    Ajout du runtime Node.js pour Prisma dans route.js

--- src/app/api/medecins/route.js
+++ src/app/api/medecins/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '#/lib/prisma';
 
+export const runtime = 'nodejs'; // Forcer l'utilisation de Node.js
 
 
 // Récupérer tous les médecins
